b46427a2926bc79a5d143318004a2f6eb1969cc7
Masaya Kemmochi <82385253+kmmch@users.noreply.github.com>
2022-01-18 22:44:37 +0900

    分割代入まで

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 /**
  * テンプレート文字列
  */
-const name = "masaya";
-const age = 24;
+// const name = "masaya";
+// const age = 24;
 
 // 従来
-const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
 // console.log(message1);
 
 // テンプレート文字列
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
 // console.log(message2);
 
 /*
@@ -19,23 +19,46 @@ const message2 = `私の名前は${name}です。年齢は${age}です。`;
 // function func1(str) {
 //   return str;
 // }
-const func1 = function (str) {
-  return str;
-};
+// const func1 = function (str) {
+//   return str;
+// };
 // console.log(func1("ああああ"));
 
 // アロー関数
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("iiiii"));
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("iiiii"));
 
 // 引数が一つの時は、引数の（）なくてもOK
 // returnを省略可能。
 // const func2 = (str) => str;
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
-};
-const func4 = (num1, num2) => num1 + num2;
-console.log(func4(10, 20));
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// const func4 = (num1, num2) => num1 + num2;
+// console.log(func4(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "masaya",
+//   age: 24
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// // console.log(message1);
+
+// const { name, age } = myProfile;
+// const message２ = `名前は${name}です。年齢は${age}です。`;
+// console.log(message２);
+
+// const myProfile = ["masaya", 24];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message4);
