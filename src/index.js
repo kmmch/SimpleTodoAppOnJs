a440f6a05f0a1cd6ba47b3dd75c0738d31c710ac
Masaya Kemmochi <82385253+kmmch@users.noreply.github.com>
2022-01-18 22:36:41 +0900

    アロー関数まで

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import "./styles.css";
+/**
+ * テンプレート文字列
+ */
+const name = "masaya";
+const age = 24;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// 従来
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// テンプレート文字列
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/*
+ * アロー関数
+ */
+// 従来
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+// console.log(func1("ああああ"));
+
+// アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("iiiii"));
+
+// 引数が一つの時は、引数の（）なくてもOK
+// returnを省略可能。
+// const func2 = (str) => str;
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+const func4 = (num1, num2) => num1 + num2;
+console.log(func4(10, 20));
