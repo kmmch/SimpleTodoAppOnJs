dc19b163f6f34801b339f0e0416a2dde839f6c9c
Masaya Kemmochi <82385253+kmmch@users.noreply.github.com>
2022-01-19 22:39:44 +0900

    スプレッド構文まで

--- src/index.js
+++ src/index.js
@@ -68,3 +68,41 @@
  */
 // const SayHello = (name = "ゲスト") => console.log(`こんにちは、${name}さん`);
 // SayHello("じゃけぇ");
+
+/**
+ * スプレッド構文
+ */
+// 配列の展開
+// const arr1 = [1, 2, 3];
+// // console.log(arr1);
+// // console.log(...arr1);
+// // ...は配列の中身を順番に取り出してくれる
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// // sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1,2,3,4,5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// console.log(arr8);
+// -> 参照が引き継がれる
+// arr8[0] = 100;
+// console.log(arr4);
