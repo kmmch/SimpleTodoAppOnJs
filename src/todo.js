d194bac555cfa1d47390dc33cfe61fa8a4268687
Masaya Kemmochi <82385253+kmmch@users.noreply.github.com>
2022-01-22 21:33:23 +0900

    タスクの完了機能

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,4 @@
+// 追加ボタンが押された時の動作
 const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputTxt = document.getElementById("add-txt").value;
@@ -16,7 +17,33 @@ const onClickAdd = () => {
   const completeBtn = document.createElement("button");
   completeBtn.innerText = "完了";
   completeBtn.addEventListener("click", () => {
-    alert("完了");
+    // 押された完了ボタンの親タグ(li)を未完了リストから削除
+    deleteFromIncompleteList(completeBtn.parentNode);
+
+    // 完了リストに追加する要素
+    const addTarget = completeBtn.parentNode;
+
+    // TODOのタイトルを取得
+    const todoTitle = addTarget.firstElementChild.innerText;
+
+    // li以下を初期化
+    addTarget.textContent = null;
+
+    // pタグ生成
+    const p = document.createElement("p");
+    p.className = "todo__ttl";
+    p.innerText = todoTitle;
+
+    // buttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    // liタグの子要素に各要素を設定
+    addTarget.appendChild(p);
+    addTarget.appendChild(backButton);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // button(削除)
@@ -24,8 +51,7 @@ const onClickAdd = () => {
   deleteBtn.innerText = "削除";
   deleteBtn.addEventListener("click", () => {
     // 押された削除ボタンの親タグ(li)を未完了リストから削除
-    const deleteTarget = deleteBtn.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncompleteList(deleteBtn.parentNode);
   });
 
   // liの子要素に書く要素を設定
@@ -35,6 +61,11 @@ const onClickAdd = () => {
 
   // 未完了のTODOのリストに追加
   document.getElementById("incomplete-list").appendChild(li);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
